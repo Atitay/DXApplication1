8da9d989ae87e6274ca8e54bffad67045b9ca669
Atitay <31137141+Atitay@users.noreply.github.com>
2019-06-24 15:02:19 +0700

    Add project files.

---
+++ GorgeShipping.Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GorgeShipping.Models
+{
+    class Address
+    {
+        public int id { get; set; }
+        public string Addresss { get; set; }
+        public string District { get; set; }
+        public string Province { get; set; }
+        public string Code { get; set; }
+        public string TelNo { get; set; }
+    }
+}
---
+++ GorgeShipping.Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GorgeShipping.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string TelNo { get; set; }
+        public bool IsLogin { get; set; }
+    }
+}
